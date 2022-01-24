e5321d953b2ca601ba0adef1ab698643bc0bd20a
Atharva Santosh Labhasetwar <64954848+atharval1@users.noreply.github.com>
2022-01-24 21:46:26 +0530

    deleted env

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-from cgitb import text
-from enum import unique
-from tkinter.tix import Form
-from unicodedata import name
 import bcrypt
 from flask import Flask, redirect, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
@@ -11,6 +7,55 @@ from wtforms.validators import InputRequired, Length, ValidationError
 from flask_wtf import FlaskForm
 from flask_bcrypt import Bcrypt
 from datetime import datetime
+
+# chnages started-----------------------------------------------------------------------------------------------------------------
+
+import requests
+import numpy as np
+import config
+import pickle
+import io
+import torch
+# from torchvision import transforms
+# from PIL import Image
+from utils.model import ResNet9
+
+# -------------------------LOADING THE TRAINED MODELS -----------------------------------------------
+
+# Loading crop recommendation model
+crop_recommendation_model_path = 'models/RandomForest.pkl'
+crop_recommendation_model = pickle.load(
+    open(crop_recommendation_model_path, 'rb'))
+
+
+
+def weather_fetch(city_name):
+    """
+    Fetch and returns the temperature and humidity of a city
+    :params: city_name
+    :return: temperature, humidity
+    """
+    api_key = config.weather_api_key
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        return temperature, humidity
+    else:
+        return None
+
+
+
+
+# chnages ended-------------------------------------------------------------------------------------------------------------
+
 
 
 app = Flask(__name__)
@@ -108,7 +153,7 @@ def dashboard():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for('hello_world'))
 
 
 @app.route("/signup",methods=['GET', 'POST'])
@@ -128,20 +173,65 @@ def signup():
 @ app.route('/crop-recommend')
 @login_required
 def crop_recommend():
-    title = 'Harvestify - Crop Recommendation'
+    title = ' - Crop Recommendation'
     return render_template('crop.html', title=title)
 
 @ app.route('/fertilizer')
 @login_required
 def fertilizer_recommendation():
-    title = 'Harvestify - Fertilizer Suggestion'
+    title = ' - Fertilizer Suggestion'
     return render_template('fertilizer.html', title=title)
 
 @app.route('/disease-predict', methods=['GET', 'POST'])
 @login_required
 def disease_prediction():
-    title = 'Harvestify - Disease Detection'
+    title = ' - Disease Detection'
     return render_template('disease.html', title=title)
+
+
+
+# chnaging started ------------------------------------------------------------------------------------------------------
+# ===============================================================================================
+
+# RENDER PREDICTION PAGES
+
+# render crop recommendation result page
+
+
+@ app.route('/crop-predict', methods=['POST'])
+def crop_prediction():
+    title = ' - Crop Recommendation'
+
+    if request.method == 'POST':
+        N = int(request.form['nitrogen'])
+        P = int(request.form['phosphorous'])
+        K = int(request.form['pottasium'])
+        ph = float(request.form['ph'])
+        rainfall = float(request.form['rainfall'])
+
+        # state = request.form.get("stt")
+        city = request.form.get("city")
+
+        if weather_fetch(city) != None:
+            temperature, humidity = weather_fetch(city)
+            data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+            my_prediction = crop_recommendation_model.predict(data)
+            final_prediction = my_prediction[0]
+
+            return render_template('crop-result.html', prediction=final_prediction, title=title)
+
+        else:
+
+            return render_template('try_again.html', title=title)
+
+# render fertilizer recommendation result page
+
+
+
+
+# chnaging ended----------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     app.run(debug=True,port=8000)
+
+
